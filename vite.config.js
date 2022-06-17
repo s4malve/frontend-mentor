3e3ea3a67119ad946ca6011f46f70db157172d6c
s4malve <s4malve@gmail.com>
2022-06-16 20:14:21 -0500

    feat: dynamic function to build every project

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+import { readdirSync } from 'fs'
+
+const addPage = () => {
+  const paths = readdirSync('projects/', (err, files) => files)
+  let obj = {
+    index: resolve(__dirname, 'index.html'),
+  }
+  paths.forEach((path) => {
+    obj = { ...obj, [path]: `projects/${path}/index.html` }
+  })
+  return obj
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    outDir: resolve(__dirname, 'dist'),
+    rollupOptions: {
+      input: addPage(),
+    },
+  },
 })
